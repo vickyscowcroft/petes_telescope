ec61ecdaee6e7871af9fbe219b252eb3647e7452
SBennett97 <43994278+SBennett97@users.noreply.github.com>
2019-02-22 12:32:20 +0000

    Update main.py

--- Project_Code/main.py
+++ Project_Code/main.py
@@ -8,6 +8,7 @@ from Project_routines import camera_routines_2 as CR
 from Project_routines import stepper_routines as MR
 import time
 import numpy as np
+from time import sleep
 
 PIXEL_LENGTH = 1024
 PIXEL_WIDTH = 768
@@ -45,7 +46,7 @@ def centralise(image_1, image_2):
     elif altitude_movement < 0:
         while altitude_movement < -5:
             MR.altMotor.singleForward(int(-4*ALT_STEPS_PER_PIXEL*altitude_movement))
-            print('negative recalculating altitude...\n')
+            print('recalculating altitude...\n')
             image_2 = CR.grey_image()
             shift = CR.drift_amount(image_1, image_2)
             altitude_movement = shift[0]
@@ -60,46 +61,50 @@ def centralise(image_1, image_2):
     
     if abs(azimuth_movement) < 5:
         pass
-    elif azimuth_movement > 0:
-        while azimuth_movement > 5:
-            MR.azMotor.singleForward(int(3*ALT_STEPS_PER_PIXEL*azimuth_movement))
+    elif azimuth_movement < 0:
+        while azimuth_movement < -5:
+            MR.azMotor.singleForward(int(-3*ALT_STEPS_PER_PIXEL*azimuth_movement))
             print('recalculating az...\n')
             image_2 = CR.grey_image()
             shift = CR.drift_amount(image_1, image_2)
             azimuth_movement = shift[1]
-    elif azimuth_movement < 0:
-        while azimuth_movement < -5:
-            MR.azMotor.singleBackward(int(-3*ALT_STEPS_PER_PIXEL*azimuth_movement))
-            print('recalculatingaz...\n')
+    elif azimuth_movement > 0:
+        while azimuth_movement > 5:
+            MR.azMotor.singleBackward(int(3*ALT_STEPS_PER_PIXEL*azimuth_movement))
+            print('recalculating az...\n')
             image_2 = CR.grey_image()
             shift = CR.drift_amount(image_1, image_2)
             azimuth_movement = shift[1]
 ##            image_2 = image_1
     print("DONE")
-    CR.capture_image_to_file('Images/final.png')
-            
-            
-##    #####CURRENTLY NO IDEA HOW THIS WORKS#####
-##    from simple_pid import PID
-##    pid = PID(1, 0.1, 0.05, setpoint=1)
-##
-##    # assume we have a system we want to control in controlled_system
-##    v = controlled_system.update(0)
-##    
-##    while True:
-##        # compute new ouput from the PID according to the systems current value
-##        control = pid(v)
-##    
-##        # feed the PID output to the system and get its current value
-##        v = controlled_system.update(control)
+    
+def main():
+    
+    print('Welcome to Jon and Seb\'s star tracking program.\n')
+    
+    begin = raw_input('Would you like to begin tracking? - (Y/N)') 
+    
+    if begin != 'N':
         
+        print('Please set the viewfinder so that the current sky is that to be tracked.\n')
+        sleep(5)
+        raw_input('When you are ready to begin tracking, press any key - ')
         
-###TEST SECTION
+        ref_image = CR.grey_image()
+        sleep(5)
+        shifted_image = CR.grey_image()
         
-image_first = CR.grey_image()
-CR.capture_image_to_file('Images/first_position.png')
-
-raw_input('Waiting for key press...')
-
-image_second = CR.grey_image()
-centralise(image_first, image_second)
+        x = 1
+        while x < 2:
+            try:
+                centralise(ref_image, shifted_image)
+                sleep(5)
+                shifted_image = CR.grey_image()
+                
+            except KeyboardInterrupt:
+                print('Tracking terminated')
+                x = 3
+                
+                
+main()
+    
